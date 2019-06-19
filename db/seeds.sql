f72ff73759931fa7c1cf93f2858f5a46df373208
janiello <janiello417@gmail.com>
2019-06-19 10:50:15 -0400

    created database schema and entries, ran code to mysql and created a config folder

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Breakfast Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Avocado Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("BBQ Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Western Burger", false);
