6c30ddb0bc7b7bcf43e04745d80b7b109a8e9768
janiello <janiello417@gmail.com>
2019-06-21 15:59:37 -0400

    coded my router after requiring express and burger.js in the controller. Exported the router object and functions to be used later to render my html via handlebars

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,8 +1,40 @@
 var express = require("express");
+
+var router = express.Router();
+
 var burger = require("../models/burger.js");
 
-var router = {
+router.get("/", function(request, response) {
+    burger.selectAll(function(data) {
+        var handlebarsObject = {
+            burgers: data
+        };
+        console.log(handlebarsObject);
+        response.render("index", handlebarsObject);
+    });
+});
 
-}
+router.post("/api/burgers", function(request, response) {
+    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+        response.json({id: result.insertId});
+    });
+});
+
+router.put("/api/burgers/:id", function(request, response) {
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
+    burger.updateOne(
+        {
+            devoured: req.body.devoured
+        },
+        condition,
+        function(result) {
+            if (result.changedRows === 0) {
+                return response.status(404).end();
+            }
+            response.status(200).end();
+        }
+    );
+});
 
 module.exports = router;
