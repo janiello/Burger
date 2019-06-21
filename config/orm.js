1f73037f3eb6efff570a3a3563f6459ba6890730
janiello <janiello417@gmail.com>
2019-06-21 12:52:41 -0400

    Updated orm.js with helper functions to make the sql commands in my functions more specific and be able to take user input

--- config/orm.js
+++ config/orm.js
@@ -1,22 +1,50 @@
 var connection = require("./connection.js");
 
+function qMarks(number) {
+    var array = [];
+    for (var i = 0; i < number; i++) {
+        array.push("?");
+    }
+    return array.toString();
+};
+
+function objectToSQL(object) {
+    var array = [];
+    for (var key in object) {
+        var value = object[key];
+        if (Object.hasOwnProperty.call(object, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            array.push(key + "=" + value);
+        }
+    }
+    return array.toString();
+};
+
 var orm = {
-    selectAll: function() {
-        connection.query("SELECT burger_name FROM burgers", function(error, result) {
-            if(error) throw error;
-            console.log(result);
+    selectAll: function(table, callback) {
+        connection.query("SELECT * FROM " + table + ";", function(error, result) {
+            if(error) {
+                throw error;
+            }
+            callback(result);
         });
     },
-    insertOne: function(burger_name) {
-        connection.query("INSERT INTO burgers (burger_name) VALUES(??)", [burger_name], function(error, result) {
-            if(error) throw error;
-            console.log(result);
-        })
+    insertOne: function(table, columns, values, callback) {
+        connection.query("INSERT INTO " + table + " (" + columns.toString() + ") VALUES (" + qMarks(values.length) + ")", values, function(error, result) {
+            if(error) {
+                throw error;
+            }
+            callback(result);
+        });
     },
-    updateOne: function(devoured) {
-        connection.query("UPDATE burgers SET devoured = true WHERE devoured = false", function(error, result) {
-            if(error) throw error;
-            console.log(result);
+    updateOne: function(table, objectColumnValues, condition, callback) {
+        connection.query("UPDATE " + table + " SET " + objectToSQL(objectColumnValues) + " WHERE " + condition, function(error, result) {
+            if(error) {
+                throw error;
+            }
+            callback(result);
         });
     }
 };
